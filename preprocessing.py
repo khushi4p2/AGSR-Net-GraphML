93fad09cf764e6911363d74178f471083cd4b427
khushi4p2 <90067755+khushi4p2@users.noreply.github.com>
2024-05-28 05:14:47 +0900

    Update preprocessing.py
    debug code - degree cent.

--- preprocessing.py
+++ preprocessing.py
@@ -37,17 +37,17 @@ def unpad(data, split):
     return train
 
 ################################## edge centrality measures #################
-def degree_centrality(A):
+def calc_degree_centrality(A):
     G = nx.from_numpy_array(A.numpy())
     centrality = nx.degree_centrality(G)
     return torch.tensor(list(centrality.values()))
 
-def eigenvector_centrality(A, max_iter=100, tol=1e-6):
+def calc_eigenvector_centrality(A, max_iter=100, tol=1e-6):
     G = nx.from_numpy_array(A.numpy())
     centrality = nx.eigenvector_centrality(G, max_iter=max_iter, tol=tol)
     return torch.tensor(list(centrality.values()))
 
-def betweenness_centrality(A):
+def calc_betweenness_centrality(A):
     G = nx.from_numpy_array(A.numpy())
     centrality = nx.edge_betweenness_centrality(G)
     return torch.tensor(list(centrality.values()))
