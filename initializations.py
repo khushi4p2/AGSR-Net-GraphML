617e0fe7e177c5ea45eed068b9cde9b9fffd84e0
khushi4p2 <90067755+khushi4p2@users.noreply.github.com>
2024-05-27 23:46:23 +0900

    Update initializations.py
    Debug code CB Poly - changed weights function

--- initializations.py
+++ initializations.py
@@ -1,11 +1,23 @@
 import numpy as np
 
 
-def weight_variable_glorot(output_dim):
+# def weight_variable_glorot(output_dim):
 
-    input_dim = output_dim
+#     input_dim = output_dim
+#     init_range = np.sqrt(6.0 / (input_dim + output_dim))
+#     initial = np.random.uniform(-init_range, init_range,
+#                                 (input_dim, output_dim))
+
+#     return initial
+
+################# modified for CB Poly 
+import numpy as np
+
+def weight_variable_glorot(input_dim, output_dim):
     init_range = np.sqrt(6.0 / (input_dim + output_dim))
     initial = np.random.uniform(-init_range, init_range,
                                 (input_dim, output_dim))
 
     return initial
+
+########################################
