5be6ec8375a28e5ba30ef32146ebb7094ae69eea
khushi4p2 <90067755+khushi4p2@users.noreply.github.com>
2024-05-27 13:51:44 +0900

    Update layers.py
    Added GAT Layer code

--- layers.py
+++ layers.py
@@ -45,27 +45,71 @@ class GSRLayer(nn.Module):
         return adj, torch.abs(X)
 
 
-class GraphConvolution(nn.Module):
-    """
-    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
-    """
+# class GraphConvolution(nn.Module):
+#     """
+#     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+#     """
 
-    def __init__(self, in_features, out_features, dropout, act=F.relu):
-        super(GraphConvolution, self).__init__()
+#     def __init__(self, in_features, out_features, dropout, act=F.relu):
+#         super(GraphConvolution, self).__init__()
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.dropout = dropout
+#         self.act = act
+#         self.weight = torch.nn.Parameter(
+#             torch.FloatTensor(in_features, out_features))
+#         self.reset_parameters()
+
+#     def reset_parameters(self):
+#         torch.nn.init.xavier_uniform_(self.weight)
+
+#     def forward(self, input, adj):
+#         input = F.dropout(input, self.dropout, self.training)
+#         support = torch.mm(input, self.weight)
+#         output = torch.mm(adj, support)
+#         output = self.act(output)
+#         return output
+
+############### Graph Attention Layer ################
+class GATLayer(nn.Module):
+    def __init__(self, in_features, out_features, dropout, alpha=0.2, concat=True):
+        super(GATLayer, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.dropout = dropout
-        self.act = act
-        self.weight = torch.nn.Parameter(
-            torch.FloatTensor(in_features, out_features))
-        self.reset_parameters()
+        self.alpha = alpha
+        self.concat = concat
 
-    def reset_parameters(self):
-        torch.nn.init.xavier_uniform_(self.weight)
+        # Define weights and a single-layer feedforward network for the attention mechanism
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))
+        nn.init.xavier_uniform_(self.weight)
+        nn.init.xavier_uniform_(self.a)
+
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
 
     def forward(self, input, adj):
+        # Apply dropout to the input features
         input = F.dropout(input, self.dropout, self.training)
-        support = torch.mm(input, self.weight)
-        output = torch.mm(adj, support)
-        output = self.act(output)
-        return output
+        # Linear transformation
+        h = torch.mm(input, self.weight)
+
+        # Prepare the attention mechanism
+        N = h.size()[0]
+        a_input = torch.cat([h.repeat(1, N).view(N * N, -1), h.repeat(N, 1)], dim=1)
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(1).view(N, N))
+
+        # Only allow non-zero attention coefficients where adj is 1 (edges exist)
+        zero_vec = -9e15*torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training)
+
+        # Weighted sum of features produced by the attention coefficients
+        h_prime = torch.matmul(attention, h)
+
+        return h_prime
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight)
+        nn.init.xavier_uniform_(self.a)
