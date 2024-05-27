7cc622d2cfa6789f98a99161736b51c5d8b64900
khushi4p2 <90067755+khushi4p2@users.noreply.github.com>
2024-05-28 04:13:55 +0900

    Update ops.py
    implemented edge centrality measures

--- ops.py
+++ ops.py
@@ -4,84 +4,6 @@ import torch.nn.functional as F
 import numpy as np
 
 
-# class GraphUnpool(nn.Module):
-
-#     def __init__(self):
-#         super(GraphUnpool, self).__init__()
-
-#     def forward(self, A, X, idx):
-#         new_X = torch.zeros([A.shape[0], X.shape[1]])
-#         new_X[idx] = X
-#         return A, new_X
-
-# class GraphPool(nn.Module):
-
-#     def __init__(self, k, in_dim):
-#         super(GraphPool, self).__init__()
-#         self.k = k
-#         self.proj = nn.Linear(in_dim, 1)
-#         self.sigmoid = nn.Sigmoid()
-
-#     def forward(self, A, X):
-#         scores = self.proj(X)
-#         # scores = torch.abs(scores)
-#         scores = torch.squeeze(scores)
-#         scores = self.sigmoid(scores/100)
-#         num_nodes = A.shape[0]
-#         values, idx = torch.topk(scores, int(self.k*num_nodes))
-#         new_X = X[idx, :]
-#         values = torch.unsqueeze(values, -1)
-#         new_X = torch.mul(new_X, values)
-#         A = A[idx, :]
-#         A = A[:, idx]
-#         return A, new_X, idx
-
-# class GraphUnet(nn.Module):
-
-#     def __init__(self, ks, in_dim, out_dim, dim=320):
-#         super(GraphUnet, self).__init__()
-#         self.ks = ks
-
-#         self.start_gcn = GCN(in_dim, dim)
-#         self.bottom_gcn = GCN(dim, dim)
-#         self.end_gcn = GCN(2*dim, out_dim)
-#         self.down_gcns = []
-#         self.up_gcns = []
-#         self.pools = []
-#         self.unpools = []
-#         self.l_n = len(ks)
-#         for i in range(self.l_n):
-#             self.down_gcns.append(GCN(dim, dim))
-#             self.up_gcns.append(GCN(dim, dim))
-#             self.pools.append(GraphPool(ks[i], dim))
-#             self.unpools.append(GraphUnpool())
-
-#     def forward(self, A, X):
-#         adj_ms = []
-#         indices_list = []
-#         down_outs = []
-#         X = self.start_gcn(A, X)
-#         start_gcn_outs = X
-#         org_X = X
-#         for i in range(self.l_n):
-
-#             X = self.down_gcns[i](A, X)
-#             adj_ms.append(A)
-#             down_outs.append(X)
-#             A, X, idx = self.pools[i](A, X)
-#             indices_list.append(idx)
-#         X = self.bottom_gcn(A, X)
-#         for i in range(self.l_n):
-#             up_idx = self.l_n - i - 1
-
-#             A, idx = adj_ms[up_idx], indices_list[up_idx]
-#             A, X = self.unpools[i](A, X, idx)
-#             X = self.up_gcns[i](A, X)
-#             X = X.add(down_outs[up_idx])
-#         X = torch.cat([X, org_X], 1)
-#         X = self.end_gcn(A, X)
-
-#         return X, start_gcn_outs
 
 ################## Attention Unpooling and Pooling ##################
 class GraphAttentionUnpool(nn.Module):
@@ -121,14 +43,15 @@ class GraphUnet(nn.Module):
         self.ks = ks
 
         ######################## GAT Layer for pooling/unpooling
-        # self.start_gcn = GCN(in_dim, dim)
-        # self.bottom_gcn = GCN(dim, dim)
-        # self.end_gcn = GCN(2*dim, out_dim)
-        # self.down_gcns = []
-        # self.up_gcns = []
-        self.start_gat = GraphAttentionLayer(in_dim, dim, dropout=0.6, alpha=0.2, concat=True)
-        self.bottom_gat = GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True)
-        self.end_gat = GraphAttentionLayer(2*dim, out_dim, dropout=0.6, alpha=0.2, concat=False)
+        ########################### edge centrality meaures ###############
+        # self.start_gat = GraphAttentionLayer(in_dim, dim, dropout=0.6, alpha=0.2, concat=True)
+        # self.bottom_gat = GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True)
+        # self.end_gat = GraphAttentionLayer(2*dim, out_dim, dropout=0.6, alpha=0.2, concat=False)
+        self.start_gat = GraphAttentionLayer(in_dim + 3, dim, dropout=0.6, alpha=0.2, concat=True)
+        self.bottom_gat = GraphAttentionLayer(dim + 3, dim, dropout=0.6, alpha=0.2, concat=True)
+        self.end_gat = GraphAttentionLayer(2*dim + 3, out_dim, dropout=0.6, alpha=0.2, concat=False)
+        ########################### edge centrality meaures ###############
+
         self.down_gats = []
         self.up_gats = []
         ###########################################################
@@ -138,12 +61,13 @@ class GraphUnet(nn.Module):
         self.l_n = len(ks)
         for i in range(self.l_n):
             ###################################### GAT Lyer for pooling/unpooling
-            # self.down_gcns.append(GCN(dim, dim))
-            # self.up_gcns.append(GCN(dim, dim))
-            # self.pools.append(GraphAttentionPool(ks[i], dim))  # Use GraphAttentionPool instead of GraphPool
-            # self.unpools.append(GraphAttentionUnpool())  # Use GraphAttentionUnpool instead of GraphUnpool
-            self.down_gats.append(GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True))
-            self.up_gats.append(GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True))
+            # self.down_gats.append(GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True))
+            # self.up_gats.append(GraphAttentionLayer(dim, dim, dropout=0.6, alpha=0.2, concat=True))
+            # self.pools.append(GraphAttentionPool(ks[i], dim))
+            # self.unpools.append(GraphAttentionUnpool())
+
+            self.down_gats.append(GraphAttentionLayer(dim + 3, dim, dropout=0.6, alpha=0.2, concat=True))
+            self.up_gats.append(GraphAttentionLayer(dim + 3, dim, dropout=0.6, alpha=0.2, concat=True))
             self.pools.append(GraphAttentionPool(ks[i], dim))
             self.unpools.append(GraphAttentionUnpool())
             ######################################################################
@@ -152,9 +76,16 @@ class GraphUnet(nn.Module):
         adj_ms = []
         indices_list = []
         down_outs = []
+
+        ################################# edge centrality 
+        degree_centrality = degree_centrality(A)
+        eigenvector_centrality = eigenvector_centrality(A)
+        betweenness_centrality = betweenness_centrality(A)
+
+        centrality_features = torch.stack([degree_centrality, eigenvector_centrality, betweenness_centrality], dim=-1)
+        X = torch.cat([X, centrality_features], dim=-1)
+        ######################################################
         
-        # X = self.start_gcn(A, X)
-        # start_gcn_outs = X
         X = self.start_gat(X, A)
         start_gat_outs = X
 
@@ -166,13 +97,6 @@ class GraphUnet(nn.Module):
             A, X, idx = self.pools[i](A, X)
             indices_list.append(idx)
         X = self.bottom_gat(X, A)
-        # for i in range(self.l_n):
-        #     X = self.down_gcns[i](A, X)
-        #     adj_ms.append(A)
-        #     down_outs.append(X)
-        #     A, X, idx = self.pools[i](A, X)
-        #     indices_list.append(idx)
-        # X = self.bottom_gcn(A, X)
         
         for i in range(self.l_n):
             up_idx = self.l_n - i - 1
@@ -180,34 +104,16 @@ class GraphUnet(nn.Module):
             A, idx = adj_ms[up_idx], indices_list[up_idx]
             A, X = self.unpools[i](A, X, idx)
             
-            # X = self.up_gcns[i](A, X)
             X = self.up_gats[i](X, A)
             
             X = X.add(down_outs[up_idx])
         X = torch.cat([X, org_X], 1)
 
-        # X = self.end_gcn(A, X)
         X = self.end_gat(X, A)
 
         return X, start_gat_outs
-        # return X, start_gcn_outs
         
-##########################################################################
-
 ########################## GAT Layrs for Pooling/Unpooling ###############
-# class GCN(nn.Module):
-
-#     def __init__(self, in_dim, out_dim):
-#         super(GCN, self).__init__()
-#         self.proj = nn.Linear(in_dim, out_dim)
-#         self.drop = nn.Dropout(p=0)
-
-#     def forward(self, A, X):
-
-#         X = self.drop(X)
-#         X = torch.matmul(A, X)
-#         X = self.proj(X)
-#         return X
 
 class GraphAttentionLayer(nn.Module):
     def __init__(self, in_features, out_features, dropout, alpha, concat=True):
